1f41a1b6aec2faed2c8ecceac47f37c1d3a785c7
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2021-01-05 15:10:23 +0200

    Final Things

--- dashboard/login.php
+++ dashboard/login.php
@@ -76,9 +76,9 @@
                     </button>
                   </form>
                   <hr>
-                  <div class="text-center">
+                  <!-- <div class="text-center">
                     <a class="small" href="register.html">Create an Account!</a>
-                  </div>
+                  </div> -->
                 </div>
               </div>
             </div>
