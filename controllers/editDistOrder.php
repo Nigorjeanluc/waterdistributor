2d02d76b572a780a2c436f35a039e6ed8e1ea3f6
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2021-01-05 10:35:14 +0200

    Final Things

--- controllers/editDistOrder.php
+++ controllers/editDistOrder.php
@@ -7,16 +7,18 @@
         $phone=$_POST['phone_number'];
         $address=$_POST['Address'];
         $quantity=$_POST['quantity'];
+        $id = $_POST['id'];
+        $processed = 0;
         $amount = $quantity * 60;
         $table = 'vieworder';
         $destinationArray = "distributor_name,phone_number,Address,quantity,processed,date";
         $sourceArray = stringCopact3($name,$phone,$address).",".stringCopact2($quantity,false);
         // $query = insertDatas($table,$destinationArray,$sourceArray);
-        $sql = "UPDATE SET distributor_name = '".$name."', phone_number = '".$phone."', Address = '".$address."', quantity = '".$quantity."', processed = false WHERE id = ".$_POST['id'];
-        // $res = mysqli_query($dbcon,$query);
-        $res2 = mysqli_query($dbcon, $sql);
-        echo $sql;
-        if($res2){
+        $sql = "UPDATE $table SET distributor_name = '$name', phone_number = '$phone', Address = '$address', quantity = '$quantity', processed = '$processed' WHERE id = $id";
+        // $res2 = mysqli_query($dbcon,$query);
+        $res = mysqli_query($dbcon, $sql);
+        // echo $sql;
+        if($res){
             echo "<meta http-equiv='refresh' content='0;url=../dashboard/stock.php?yes=0#here'>";
         }else{
             echo "<meta http-equiv='refresh' content='0;url=../dashboard/stock.php?no=0'>";
--- nav.php
+++ nav.php
@@ -7,11 +7,30 @@
                 $sql = mysqli_query($dbcon,"SELECT * FROM orders WHERE processed=true AND customerName='".$_SESSION['user']."' ORDER BY date DESC");
                 echo '
                 <li class="drop-down"><a href="#"><i class="fas fa-bell fa-fw"></i> Notifications</a>
-                    <ul>';
+                    <ul class="list-group">';
                 
                     while($row = mysqli_fetch_array($sql)){
-                        echo "<li style='padding: 5px'>".$row['customerName'].", you have ".$row['quantity']." litres <br />given at ".$row['date']."</li>";
-                        echo "<hr />";
+                        echo "<li class='list-group-item'>";
+                        switch($row['quantity']) {
+                            case 20:
+                                echo '<img style="width:50px" src="assets/img/20litre.PNG" />';
+                                break;
+                            case 19:
+                                echo '<img style="width:50px" src="assets/img/9litre.PNG" />';
+                                break;
+                            case 7:
+                                echo '<img style="width:50px" src="assets/img/7litres.PNG" />';
+                                break;
+                            case 5:
+                                echo '<img style="width:50px" src="assets/img/5litre.PNG" />';
+                                break;
+                            case 1:
+                                echo '<img style="width:50px" src="assets/img/litre.PNG" />';
+                                break;
+                            default:
+                                echo '<img style="width:50px" src="assets/img/20litre.PNG" />';
+                        }
+                        echo $row['customerName'].", you have ".$row['quantity']." litres <br />given at ".$row['date']."</li>";
                     }
                 echo '
                     </ul>
