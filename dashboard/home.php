f77fad57e986a4ac3442f43ac75d91e938c0a053
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2021-01-05 15:14:56 +0200

    Final Things

--- dashboard/home.php
+++ dashboard/home.php
@@ -78,7 +78,7 @@
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <!-- <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -91,7 +91,7 @@
                   </div>
                 </div>
               </div>
-            </div>
+            </div> -->
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
