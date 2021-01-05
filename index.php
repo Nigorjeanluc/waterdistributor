a8c82c3fbc06c8164443987dd57afb50fee937c0
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2021-01-05 10:48:34 +0200

    Final Things

--- index.php
+++ index.php
@@ -65,39 +65,35 @@
   <main id="main">
 
     <!-- ======= Clients Section ======= -->
-    <!-- <section id="clients" class="clients section-bg">
+    <section id="clients" class="clients section-bg">
       <div class="container">
 
         <div class="row">
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+            <img src="assets/img/clients/20litre.PNG" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="assets/img/clients/client-2.png" class="img-fluid" alt="">
+            <img src="assets/img/9litre.PNG" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
+            <img src="assets/img/7litres.PNG" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="assets/img/clients/client-4.png" class="img-fluid" alt="">
+            <img src="assets/img/5litre.PNG" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="assets/img/clients/client-5.png" class="img-fluid" alt="">
-          </div>
-
-          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="assets/img/clients/client-6.png" class="img-fluid" alt="">
+            <img src="assets/img/litre.PNG" class="img-fluid" alt="">
           </div>
 
         </div>
 
       </div>
-    </section> -->
+    </section>
     <!-- End Clients Section -->
 
     <!-- ======= About Section ======= -->
